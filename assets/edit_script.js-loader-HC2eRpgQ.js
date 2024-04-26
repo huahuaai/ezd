0fd2c612bdb1eae322fb29121c4c2dc97a760976
huahua <huazixu1998@gmail.com>
2024-04-26 16:11:45 +0800

    commit:注入按钮bug修复  导入图片逻辑修改 修改左右滑动UI 修改字体为公众号网站字体

--- assets/edit_script.js-loader-HC2eRpgQ.js
+++ assets/edit_script.js-loader-HC2eRpgQ.js
@@ -3,18 +3,9 @@
 
   const injectTime = performance.now();
   (async () => {
-    if ("")
-      await import(
-        /* @vite-ignore */
-        chrome.runtime.getURL("")
-      );
-    await import(
-      /* @vite-ignore */
-      chrome.runtime.getURL("vendor/vite-client.js")
-    );
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("src/content_scripts/material_script.js.js")
+      chrome.runtime.getURL("assets/edit_script.js-DsdhHTuI.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
--- service-worker-loader.js
+++ service-worker-loader.js
@@ -1,3 +1,1 @@
-import 'http://localhost:5173/@vite/env';
-import 'http://localhost:5173/@crx/client-worker';
-import 'http://localhost:5173/src/background.js';
+import './assets/background.js-tJwSrwQQ.js';
