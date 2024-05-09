585a749bb0132833921167f6e5a9f45ed40e8152
huahua <huazixu1998@gmail.com>
2024-05-09 20:19:03 +0800

    撤回重做功能 视频音频视频号红包直播预约等组件

--- assets/edit_script.js-loader-NoVG8mCX.js
+++ assets/edit_script.js-loader-NoVG8mCX.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/edit_script.js-DsdhHTuI.js")
+      chrome.runtime.getURL("assets/edit_script.js-DL5wlpre.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
--- assets/material_script.js-loader-CKfDjc0E.js
+++ assets/material_script.js-loader-CKfDjc0E.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/material_script.js-CCKBACoK.js")
+      chrome.runtime.getURL("assets/material_script.js-vMQofgBd.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
