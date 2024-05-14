f4e1ef34862e7ba2b3dc29995c854d131fade020
huahua <huazixu1998@gmail.com>
2024-05-14 23:52:44 +0800

    视频组件修复 增加导入音乐卡片功能 加入导入代码功能

--- assets/edit_script.js-loader-B6da6HtP.js
+++ assets/edit_script.js-loader-B6da6HtP.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/edit_script.js-C8zKqlAT.js")
+      chrome.runtime.getURL("assets/edit_script.js-WwzSUEO1.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
