262bbe4ce4d9d754c7cadce8c94290178083d4af
huahua <huazixu1998@gmail.com>
2024-07-30 12:31:58 +0800

    2024/07/30 betav1.1

--- assets/edit_script.js-loader-CKObnw_K.js
+++ assets/edit_script.js-loader-CKObnw_K.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/edit_script.js-B4NwwXPn.js")
+      chrome.runtime.getURL("assets/edit_script.js-CQ3j6lCt.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
